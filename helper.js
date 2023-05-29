dd1023303fb51cb8e9d40914ec635cc96201e11c
ojvwe <ojvwebdesign@gmail.com>
2023-05-29 19:25:47 -0400

    first commit

---
+++ helper.js
@@ -0,0 +1,30 @@
+function choice(items) {
+    let idx = Math.floor(Math.random() * items.length);
+    return items[idx];
+}
+
+function remove(item, items) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === item) {
+            return [...items.slice(0, i), ...items.slice(i + 1)];
+        }
+    }
+}
+
+
+export { choice, remove };
+
+function choice(items) {
+    let idx = Math.floor(Math.random() * items.length);
+    return items[idx];
+}
+
+function remove(item, items) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === item) {
+            return [...items.slice(0, i), ...items.slice(i + 1)];
+        }
+    }
+}
+
+export { choice, remove };
